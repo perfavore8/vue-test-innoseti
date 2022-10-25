ec5355fcedf49f9000de5ba976bc6a24d56f9086
perfavore8 <nekit2353@mail.ru>
2022-10-25 21:15:25 +0200

    first commit

---
+++ js/about.50c94fcb.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvue_test_innoseti"]=self["webpackChunkvue_test_innoseti"]||[]).push([[443],{100:function(t,e,n){n.r(e),n.d(e,{default:function(){return r}});var u=function(){var t=this,e=t._self._c;return e("div",{staticClass:"about"},[t._t("default")],2)},s=[],i=n(1),l={},a=(0,i.Z)(l,u,s,!1,null,"5e032e98",null),r=a.exports}}]);
+//# sourceMappingURL=about.50c94fcb.js.map
